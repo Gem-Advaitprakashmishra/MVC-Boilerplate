3509e08070d0a82c10dc061ed10e4a41d9923cbf
Advait <Advait.Mishra@geminiSolutions.com>
2022-11-15 11:04:57 +0530

    s

---
+++ Assignment1/Assignment1/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Assignment1.View;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            binddatagrid();
+        }
+        private string CnnStr = ("Data Source = 10.50.18.16; Initial Catalog = training_db; User ID = SVC_TRANING; Password = Gemini@123;");
+
+        private void binddatagrid()
+        {
+            SqlConnection Con = new SqlConnection(CnnStr);
+
+            Con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "Select * from UserDetail";
+            cmd.Connection = Con;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable("UserDetail");
+            da.Fill(dt);
+
+            g1.ItemsSource = dt.DefaultView;
+
+
+        }
+        private void g1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoginView sys = new LoginView();
+            Close();
+            
+        }
+    }
+}
---
+++ Assignment1/Assignment1/ViewModel/LoginViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Assignment1.View;
+
+namespace Assignment1.ViewModel
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+    
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private string UserName;
+        public string username
+        {
+            get { return UserName; }
+            set
+            {
+                UserName = value;
+                NotifyPropertyChanged("username");
+            }
+        }
+
+        private string Password;
+        public string password
+        {
+            get { return Password; }
+            set
+            {
+                Password = value;
+                NotifyPropertyChanged("password");
+            }
+        }
+        private string errorMessage;
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set
+            {
+                errorMessage = value;
+                NotifyPropertyChanged("ErrorMessage");
+            }
+        }
+
+        public ICommand cmdlogin { get; private set; }
+        public bool CanExectute
+        {
+            get { return !string.IsNullOrEmpty(UserName) & !string.IsNullOrEmpty(Password); }
+        }
+
+        private string Connectionstring = ("Data Source = 10.50.18.16; Initial Catalog = training_db; User ID = SVC_TRANING; Password = Gemini@123; TrustServerCertificate=True");
+        public LoginViewModel()
+        {
+            cmdlogin = new Command(Save, () => CanExectute);
+        }
+
+        private void Save()
+        {
+
+            SqlConnection con = new SqlConnection(Connectionstring);
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandText = " Select * from UserDetail where username = @UserName and password = @Password";
+            cmd.Parameters.AddWithValue("@UserName", username);
+            cmd.Parameters.AddWithValue("@Password", password); cmd.CommandType = CommandType.Text;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet);
+            if (dataSet.Tables[0].Rows.Count > 0)
+            {
+                MainWindow m = new MainWindow();
+                m.Show();
+                con.Close();
+                username = "";
+                password = "";
+                LoginView login = new LoginView();
+                login.Close();
+            }
+            else
+            {
+                ErrorMessage = "No account registerd with these details";
+            }
+
+
+        }
+    }
+}
+
+
